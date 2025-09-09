1c49078fadc6a78a1be88c0c2e5ad24a03dd350d
Anandhu V S <102361715+Anandhu362@users.noreply.github.com>
2025-09-09 10:39:56 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Weather from './components/Weather.jsx';
+// import ProductCard from './components/ProductCard';
+// import ProductList from './components/ProductList';
+function App() {
+  return (
+    <div className='App'>
+      <Weather />
+      </div>
+    // <div className="App">
+    //   <ProductList />
+    // </div>
+  );
+}
+export default App;
